17bccf19a5aeccde482622353a44df0824f8fde4
AaqibQureshi <Aaqibqureshi321@gmail.com>
2023-11-12 21:20:39 +0500

    project-setup

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import WelcomeScreen from "./src/screens/WelcomeScreen/WelcomeScreen";
+import {AppNavigator} from "./src/navigation/AppNavigator";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        // <NavigationContainer>
+            <View style={styles.container}>
+              <AppNavigator />
+                {/*<StatusBar style="auto" />*/}
+            </View>
+        // </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        // backgroundColor: '#fff',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
 });
